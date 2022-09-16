647a841911c1083da76d17505d85922ff457cc9f
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-09-16 09:53:00 +0000

    Created array of filenames and alt text

--- main.js
+++ main.js
@@ -6,7 +6,21 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+let pictures = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg",
+    "images/pic4.jpg",
+    "images/pic5.jpg"
+]
+
 /* Declaring the alternative text for each image file */
+document.getElementById("pic1").alt = "Closeup of a human eye";
+document.getElementById("pic2").alt = "A pale brown sand dune";
+document.getElementById("pic3").alt = "Purple and white flowers";
+document.getElementById("pic1").alt = "Closeup of a human eye";
+document.getElementById("pic1").alt = "Closeup of a human eye";
+
 
 /* Looping through images */
 
